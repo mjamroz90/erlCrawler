15948e0c5dc6406754077e89c926c3a99dc43c28
mjamroz <mjamroz@mjamroz-Latitude-D520.(none)>
2012-04-09 16:52:28 +0200

    zaimplementowalen disk_cache_server oraz test do niego

---
+++ cache/src/disk_cache_server.erl
@@ -0,0 +1,85 @@
+-module(disk_cache_server).
+-export([start/2,insert/2,lookup/1,update/2,delete/1,change_host/2,stop/0]).
+-record(state,{nodename, port, riakc_pid}).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+    
+-define(URL_BUCKET,term_to_binary("Urls")).
+    
+%======================================API==================================
+         
+start(NodeName,Port) ->
+	gen_server:start_link({local,?MODULE},?MODULE,[NodeName,Port],[]).
+	
+insert(Url,Params) ->
+	gen_server:cast(?MODULE,{insert,{Url,Params}}).
+	
+lookup(Url) ->
+	gen_server:call(?MODULE,{lookup,Url}).
+	
+update(Url,Params) ->
+	gen_server:cast(?MODULE,{update,{Url,Params}}).
+	
+delete(Url) ->
+	gen_server:cast(?MODULE,{delete,Url}).
+	
+change_host(NewNodeName,NewPort) ->
+	gen_server:call(?MODULE,{change_host,{NewNodeName,NewPort}}).
+	
+stop() ->
+	gen_server:cast(?MODULE,stop).
+	
+%===================================Callbacks==============================
+
+init([NodeName,Port]) ->	
+	case riakc_pb_socket:start_link(NodeName,Port) of
+		{ok,Pid} -> {ok,#state{riakc_pid = Pid}};
+		{error,Reason} -> {stop,Reason}
+	end.	
+	
+handle_cast({insert,{Url,Params}},State = #state{riakc_pid = Pid}) ->
+	Object = riakc_obj:new(?URL_BUCKET,term_to_binary(Url),term_to_binary(Params)),
+	riakc_pb_socket:put(Pid,Object),
+	{noreply,State};	
+	
+handle_cast({update,{Url,Params}},State = #state{riakc_pid = Pid}) ->
+	{ok,Obj} = riakc_pb_socket:get(Pid,?URL_BUCKET,term_to_binary(Url)),
+	Updated_Obj = riakc_obj:update_value(Obj,term_to_binary(Params)),
+	riakc_pb_socket:put(Pid,Updated_Obj),
+	{noreply,State};
+	
+handle_cast({delete,Url},State = #state{riakc_pid = Pid}) ->
+	riakc_pb_socket:delete(Pid,?URL_BUCKET,term_to_binary(Url)),
+	{noreply,State};
+	
+handle_cast(stop,State) ->
+	{stop,"Made to stop",State}.
+	
+handle_call({lookup,Url},_From,State = #state{riakc_pid = Pid}) ->
+	Result = case riakc_pb_socket:get(Pid,?URL_BUCKET,term_to_binary(Url)) of
+		{ok,Object} -> 
+			binary_to_term(riakc_obj:get_value(Object));
+			
+		{error,_} -> not_found
+	end,
+	{reply,Result,State};	
+ 
+handle_call({change_host,{NewNodeName,NewPort}},_From,State = #state{riakc_pid=Pid}) ->
+	Result = case riakc_pb_socket:start_link(NewNodeName,NewPort) of
+		{ok,NewPid} -> ok;
+		
+		{error,Reason} -> NewPid = Pid,{error,Reason}
+	end,
+	{reply,Result,State#state{nodename=NewNodeName,port=NewPort,riakc_pid=NewPid}}.
+	
+handle_info(_Msg,State) ->
+	{noreply,State}.
+	
+terminate(_Reason,#state{riakc_pid = Pid}) ->
+	riakc_pb_socket:stop(Pid),
+	ok.
+	
+code_change(_OldVsn,State,_Extra) ->
+	{ok,State}.
+	
+	
---
+++ cache/test/disk_cache_server_test.erl
@@ -0,0 +1,33 @@
+-module(disk_cache_server_test).
+-include_lib("eunit/include/eunit.hrl").
+-define(NODENAME,"127.0.0.1").
+-define(PORT,8087).
+-compile(export_all).
+
+disk_cache_server_test_() ->
+	{setup,
+	fun setup/0,
+	fun cleanup/1,
+	[
+		fun test_function/0
+	]
+	}.
+	
+setup() ->
+	disk_cache_server:start(?NODENAME,?PORT).
+	
+cleanup(_) ->
+	disk_cache_server:stop().
+	
+test_function() ->
+	Seq = lists:seq(1,100),
+	lists:foreach(fun(X) -> disk_cache_server:insert(make_url(X),{{width,X},{depth,X*2}}) end, Seq),
+	lists:foreach(fun(X) -> ?assertEqual({{width,X},{depth,X*2}},disk_cache_server:lookup(make_url(X))) end, Seq),
+	?assertEqual(not_found,disk_cache_server:lookup("www.abcd.pl")),
+	lists:foreach(fun(X) -> disk_cache_server:update(make_url(X),{{width,2*X},{depth,X}}) end, Seq),
+	lists:foreach(fun(X) -> ?assertEqual({{width,2*X},{depth,X}},disk_cache_server:lookup(make_url(X))) end, Seq),
+	lists:foreach(fun(X) -> disk_cache_server:delete(make_url(X)) end, Seq),
+	lists:foreach(fun(X) -> ?assertEqual(not_found,disk_cache_server:lookup(make_url(X))) end, Seq).	
+	
+make_url(Num) ->
+	"www.agh.edu.pl"++lists:flatten(io_lib:format("~p", [Num])).
